d49305aa0ab52060a31ffffda075b03f066ab429
MrHidalgo <Luckyhidalgo@gmail.com>
2016-04-03 08:48:30 +0300

    Example doc/carousel - not adap/resp

--- src/script/main.js
+++ src/script/main.js
@@ -7,3 +7,28 @@ $('.header-lang a').click( function(e) {
     $('.header-lang a').removeClass('active');
     $(this).addClass('active');
 });
+
+/*
+*   SLIDER
+*/
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel(
+        {
+            loop:true,
+            margin:10,
+            // nav:true,
+            responsiveClass:true,
+            responsive:{
+                0:{
+                    items:2
+                },
+                768:{
+                    items:3
+                },
+                1000:{
+                    items:5
+                }
+            }
+        }
+    );
+});
