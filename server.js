2bd1616f1c188021aa72231dc2f1722bc7fe8e60
MrHidalgo <Luckyhidalgo@gmail.com>
2016-04-06 00:08:23 +0300

    Security  (!footer)

--- server.js
+++ server.js
@@ -2,8 +2,28 @@ var	express	    =	require('express'),
     app         =	express();
 
 app.set('port',	(process.env.PORT	||	4000));
-app.use('/scope',	express.static('./dist/page/', {'index' : 'scope.html'}));
-app.use('/',	express.static('./dist/', {'index' : 'index.html'}));
+
+// SCOPE PAGE
+app.use('/scope',	    express.static('./dist/page/',
+    {
+        'index' : 'scope.html'
+    }
+));
+
+// SECURITY PAGE
+app.use('/security',	express.static('./dist/page/',
+    {
+        'index' : 'security.html'
+    }
+));
+
+// MAIN PAGE
+app.use('/',	        express.static('./dist/',
+    {
+        'index' : 'index.html'
+    }
+));
+
 
 app.listen(app.get('port'),	function()	{
     console.log('Server	started:	http://localhost:'	+	app.get('port')	+	'/');
