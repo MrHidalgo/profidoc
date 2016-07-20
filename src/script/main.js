ab82ebc3c144c5ecac5fb48e12a90274b7e559db
MrHidalgo <Luckyhidalgo@gmail.com>
2016-07-20 20:59:06 +0300

    Apad - v1.01 & range

--- src/script/main.js
+++ src/script/main.js
@@ -178,7 +178,25 @@ $(window).on('load resize', function() {
 $(document).ready(
 	function () {
 
+		var rangeSlider = function(){
+			var slider = $('.range-slider'),
+				range = $('.range-slider__range'),
+				value = $('.range-slider__value');
 
+			slider.each(function(){
+
+				value.each(function(){
+					var value = $(this).prev().attr('value');
+					$(this).html(value);
+				});
+
+				range.on('input', function(){
+					$(this).next(value).html(this.value);
+				});
+			});
+		};
+
+		rangeSlider();
 
 
 	    // btn menu personal area
